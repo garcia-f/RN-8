364331577a0007144258cc21759c1885731b23cb
Fabian <garciafabianemmanuel@gmail.com>
2024-08-21 09:56:31 -0300

    finalizacion del tp8

--- App.js
+++ App.js
@@ -1,54 +1,95 @@
+import { Stack } from 'expo-router';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, TouchableOpacity, View, Dimensions } from 'react-native';
+import Animated, { Easing, useSharedValue, useAnimatedStyle, withTiming } from 'react-native-reanimated';
 
+export default function Index() {
 
+  const backgroundOpacity = useSharedValue(1);
+  const titleOpacity = useSharedValue(1);
+  const translateY = useSharedValue(-300);
+  const [isFaded, setIsFaded] = useState(false);
 
-import Animated, {
-  useSharedValue,
-  withTiming,
-  useAnimatedStyle,
-  Easing,
-} from "react-native-reanimated";
-import { View, Button, StyleSheet, Text } from "react-native";
-
-
-export default function AnimatedStyleUpdateExample(props) {
-  const randomWidth = useSharedValue(10);
-
-  const config = {
-    duration: 500,
-    easing: Easing.bezier(0.5, 0.01, 0, 1),
-  };
-
-  const style = useAnimatedStyle(() => {
+  const backgroundStyle = useAnimatedStyle(() => {
     return {
-      width: withTiming(randomWidth.value, config),
+      opacity: backgroundOpacity.value,
     };
   });
 
+  const titleStyle = useAnimatedStyle(() => {
+    return {
+      opacity: titleOpacity.value,
+      transform: [{ translateY: translateY.value }],
+    };
+  });
+
+  useEffect(() => {
+    translateY.value = withTiming(30, { duration: 1000, easing: Easing.inOut(Easing.linear) });
+  }, []);
+
+  const handlePress = () => {
+    if (isFaded) {
+      backgroundOpacity.value = withTiming(1, {
+        duration: 2000,
+        easing: Easing.inOut(Easing.ease),
+      });
+      titleOpacity.value = withTiming(1, {
+        duration: 2000,
+        easing: Easing.inOut(Easing.ease),
+      });
+      translateY.value = withTiming(30, { duration: 1000, easing: Easing.inOut(Easing.linear) });
+    } else {
+      backgroundOpacity.value = withTiming(0.5, {
+        duration: 2000,
+        easing: Easing.inOut(Easing.ease),
+      });
+      titleOpacity.value = withTiming(0, {
+        duration: 2000,
+        easing: Easing.inOut(Easing.ease),
+      });
+      translateY.value = withTiming(-300, { duration: 1000, easing: Easing.inOut(Easing.linear) });
+    }
+    setIsFaded(!isFaded);
+  };
+
   return (
-    <View style={styles.contenedor}>
-      <Text>Trabajo Practico N8</Text>
-      <Animated.View
-        style={[
-          { width: 100, height: 80, backgroundColor: "black", margin: 30 },
-          style,
-        ]}
-      />
-      <Button
-        title="Iniciar"
-        onPress={() => {
-          randomWidth.value = Math.random() * 350;
-        }}
-      />
+    <View style={styles.container}>
+      <Animated.View style={[styles.background, backgroundStyle]} />
+      <Animated.View style={[styles.box, titleStyle]}>
+        <Text style={styles.title}>Trabajo practico</Text>
+        <Text style={styles.title}>N8</Text>
+      </Animated.View>
+      <TouchableOpacity style={styles.btn} onPress={handlePress}>
+        <Text>{isFaded ? 'Restaurar Fondo' : 'Iniciar'}</Text>
+      </TouchableOpacity>
     </View>
   );
-}
-
-
+};
 const styles = StyleSheet.create({
-  contenedor: {
-      flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-        flexDirection: "column",
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  background: {
+    ...StyleSheet.absoluteFillObject,
+    backgroundColor: 'brown',
+  },
+  box: {
+    width: 200,
+    height: 100,
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  btn: {
+    backgroundColor: '#6f9df9',
+    padding: 10,
+    borderRadius: 10,
+    marginTop: '15%',
   },
 });
